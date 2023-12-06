186b87a40a6fd0b055c3d9a2aac5737af5c1eefd
Gaspare Monte <gasmo92@gmail.com>
2023-12-06 10:38:49 +0100

    feat: display video for each exercise

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -6,23 +6,33 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ExerciseView: View {
     let index: Int
 
-    var exercises: Exercise {
+    var exercise: Exercise {
         Exercise.exercises[index]
     }
 
     var body: some View {
-        VStack {
-            HeaderView(exerciseName: exercises.exerciseName)
+        GeometryReader { geometry in
+            VStack {
+                HeaderView(exerciseName: exercise.exerciseName)
+                    .padding(.bottom)
 
-            Text("Video player")
-            Text("Time")
-            Text("Start/done Button")
-            Text("Rating")
-            Text("History button")
+                if let url = Bundle.main.url(forResource: exercise.videoName, withExtension: "mp4") {
+                    VideoPlayer(player: AVPlayer(url: url))
+                        .frame(height: geometry.size.height * 0.45)
+                } else {
+                    Text("Couldn't find \(exercise.videoName).mp4")
+                      .foregroundColor(.red)
+                }
+                Text("Time")
+                Text("Start/done Button")
+                Text("Rating")
+                Text("History button")
+            }
         }
     }
 }
