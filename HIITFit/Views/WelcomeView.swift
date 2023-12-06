5cb6cb9953ead9bffaf953bfc29346b6d0c8f724
Gaspare Monte <gasmo92@gmail.com>
2023-12-06 12:23:47 +0100

    feat: style WelcomeView

--- HIITFit/Views/WelcomeView.swift
+++ HIITFit/Views/WelcomeView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HeaderView(titleText: "Welcome")
+                    .padding(.bottom)
+
+                Spacer()
+
+                Button("History") {
+
+                }
+                .padding(.bottom)
+            }
+
+            VStack {
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("Get fit")
+                            .font(.largeTitle)
+                        Text("with high intensity interval training")
+                            .font(.headline)
+                    }
+
+                    Image("step-up")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 240, height: 240)
+                        .clipShape(Circle())
+                }
+            }
+        }
     }
 }
 
