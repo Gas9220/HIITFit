1213bab5520e22e81955a210b37d531f2243a4af
Gaspare <Gas9220@users.noreply.github.com>
2023-12-11 13:05:35 +0100

    feat: improve exercise timer

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -10,70 +10,20 @@ import SwiftUI
 struct ExerciseView: View {
     @EnvironmentObject var history: HistoryStore
 
-    @Binding var selectedTab: Int
-
     @State private var showHistory = false
     @State private var showSuccess = false
     @State private var timerDone = false
     @State private var showTimer = false
+
+    @Binding var selectedTab: Int
 
     let index: Int
 
     var exercise: Exercise {
         Exercise.exercises[index]
     }
-
     var lastExercise: Bool {
         index + 1 == Exercise.exercises.count
-    }
-
-    var body: some View {
-        GeometryReader { geometry in
-
-            VStack(spacing: 0) {
-
-                HeaderView(selectedTab: $selectedTab, titleText: exercise.exerciseName)
-                    .padding(.bottom)
-
-                ContainerView {
-                    VStack {
-                        VideoPlayerView(videoName: exercise.videoName)
-                            .frame(height: geometry.size.height * 0.35)
-                            .padding(20)
-
-                        HStack(spacing: 150) {
-                            startButton
-                            doneButton
-                                .disabled(!timerDone)
-                                .sheet(isPresented: $showSuccess) {
-                                    SuccessView(selectedTab: $selectedTab)
-                                        .presentationDetents([.medium, .large])
-                                }
-                        }
-                        .font(.title3)
-                        .padding()
-
-                        if showTimer {
-                            TimerView(timerDone: $timerDone, size: geometry.size.height * 0.07)
-                        }
-
-                        Spacer()
-
-                        RatingView(exerciseIndex: index)
-                            .padding()
-
-                        Button("History") {
-                            showHistory.toggle()
-                        }
-                        .padding(.bottom)
-                        .sheet(isPresented: $showHistory) {
-                            HistoryView(showHistory: $showHistory)
-                        }
-
-                    }
-                }
-            }
-        }
     }
 
     var startButton: some View {
@@ -85,16 +35,82 @@ struct ExerciseView: View {
     var doneButton: some View {
         Button("Done") {
             history.addDoneExercise(Exercise.exercises[index].exerciseName)
+            timerDone = false
+            showTimer.toggle()
+            if lastExercise {
+                showSuccess.toggle()
+            } else {
+                selectedTab += 1
+            }
+        }
+    }
 
-            withAnimation {
-                if lastExercise {
-                    showSuccess.toggle()
-                } else {
-                    selectedTab += 1
+    var historyButton: some View {
+        Button(
+            action: {
+                showHistory = true
+            }, label: {
+                Text("History")
+                    .fontWeight(.bold)
+                    .padding([.leading, .trailing], 5)
+            })
+        .padding(.bottom, 10)
+        .buttonStyle(EmbossedButtonStyle())
+    }
+
+    var body: some View {
+        let exerciseName = Exercise.exercises[index].exerciseName
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                HeaderView(selectedTab: $selectedTab, titleText: exerciseName)
+                .padding(.bottom)
+                Spacer()
+                ContainerView {
+                    VStack {
+                        VideoPlayerView(videoName: exercise.videoName)
+                            .frame(height: geometry.size.height * 0.35)
+                            .padding(20)
+                        
+                        HStack(spacing: 150) {
+                            startButton
+                                .padding([.leading, .trailing], geometry.size.width * 0.1)
+                                .sheet(isPresented: $showTimer) {
+                                    TimerView(timerDone: $timerDone, exerciseName: exerciseName)
+                                        .onDisappear {
+                                            if timerDone {
+                                                history.addDoneExercise(Exercise.exercises[index].exerciseName)
+                                                timerDone = false
+                                                if lastExercise {
+                                                    showSuccess.toggle()
+                                                } else {
+                                                    withAnimation {
+                                                        selectedTab += 1
+                                                    }
+                                                }
+                                            }
+                                        }
+                                }
+                                .sheet(isPresented: $showSuccess) {
+                                    SuccessView(selectedTab: $selectedTab)
+                                        .presentationDetents([.medium, .large])
+                                }
+                        }
+                        .font(.title3)
+                        .padding()
+
+                        Spacer()
+
+                        RatingView(exerciseIndex: index)
+                            .padding()
+
+                        historyButton
+                            .sheet(isPresented: $showHistory) {
+                                HistoryView(showHistory: $showHistory)
+                            }
+                            .padding(.bottom)
+                    }
                 }
-
-                timerDone = false
-                showTimer.toggle()
+                .frame(height: geometry.size.height * 0.8)
             }
         }
     }
